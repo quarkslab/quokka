58f744530d537e7fea4628980eb1147c06d2c7e5
A. Challande <achallande@quarkslab.com>
2022-11-29 10:55:10 +0100

    feat: Bump to 0.0.5

--- bindings/python/quokka/__init__.py
+++ bindings/python/quokka/__init__.py
@@ -19,8 +19,8 @@ itself.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-__version__ = "1.0.4"
-__quokka_version__ = "0.0.4"
+__version__ = "1.0.5"
+__quokka_version__ = "0.0.5"
 
 import quokka.analysis
 import quokka.backends
