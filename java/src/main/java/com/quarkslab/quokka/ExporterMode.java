7004d3d12dc68933786daf76bc5ecda13f87ce21
Riccardo Mori <patacca@autistici.org>
2023-05-30 17:09:51 +0200

    [ghidra] Export the metadata and write to file

--- java/src/main/java/com/quarkslab/quokka/ExporterMode.java
+++ java/src/main/java/com/quarkslab/quokka/ExporterMode.java
@@ -1,5 +1,8 @@
 package com.quarkslab.quokka;
 
+import javax.management.RuntimeErrorException;
+import quokka.QuokkaOuterClass.Quokka.ExporterMeta.Mode;
+
 public enum ExporterMode {
     MODE_LIGHT, MODE_NORMAL, MODE_FULL;
 
@@ -7,5 +10,18 @@ public enum ExporterMode {
         String[] parts = this.name().split("_");
         String part = parts[1].substring(1).toLowerCase();
         return Character.toUpperCase(parts[1].charAt(0)) + part + " " + parts[0].toLowerCase();
+    }
+
+    public Mode toProto() {
+        switch (values()[ordinal()]) {
+            case MODE_LIGHT:
+                return Mode.MODE_LIGHT;
+            case MODE_NORMAL:
+                return Mode.MODE_NORMAL;
+            case MODE_FULL:
+                return Mode.MODE_FULL;
+            default:
+                throw new RuntimeException("Cannot convert unknown value");
+        }
     }
 }
