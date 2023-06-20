7270339383bb8cf3b1c6da0d80e0c801db389bdb
Riccardo Mori <patacca@autistici.org>
2023-06-20 15:45:35 +0200

    Update license to 2023

--- bindings/python/quokka/data.py
+++ bindings/python/quokka/data.py
@@ -3,7 +3,7 @@
 A data is a piece of information that isn't code.
 """
 
-#  Copyright 2022 Quarkslab
+#  Copyright 2022-2023 Quarkslab
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
--- bindings/python/quokka/reference.py
+++ bindings/python/quokka/reference.py
@@ -12,7 +12,7 @@ i.e. how to resolve the pointed object.
 
 There is room for improvement here. ;)
 """
-#  Copyright 2022 Quarkslab
+#  Copyright 2022-2023 Quarkslab
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
