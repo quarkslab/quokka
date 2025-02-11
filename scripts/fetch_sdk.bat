86ed3812b9e22a8abebda9802bff5588f5313262
Riccardo Mori <patacca@autistici.org>
2025-02-11 14:19:51 +0100

    Fix windows script

--- scripts/fetch_sdk.bat
+++ scripts/fetch_sdk.bat
@@ -2,13 +2,13 @@
 
 if not exist "..\third_party" mkdir ..\third_party
 
-curl -o ..\third_party\idasdk74.zip http://files.quarkslab.com/22262c8a-f73c-4571-b7cb-7d2b0abdf8df/idasdk74.zip
-curl -o ..\third_party\idasdk77.zip https://files.quarkslab.com/c6116218-52da-4c7d-b7ad-07a19fd6b075/idasdk77.zip
-curl -o ..\third_party\idasdk80.zip https://files.quarkslab.com/b91adc05-abdc-44ef-a1e2-6a3ccccc4c24/idasdk80.zip
-curl -o ..\third_party\idasdk81.zip https://files.quarkslab.com/cf1c6814-6304-466b-afa7-e02dab13c456/idasdk81.zip
-curl -o ..\third_party\idasdk82.zip https://files.quarkslab.com/96048f23-abb7-4f13-aeda-38ac156cea4e/idasdk82.zip
-curl -o ..\third_party\idasdk83.zip https://files.quarkslab.com/c2c793ae-7a42-4c91-aee7-725547547d3e/idasdk83.zip
-curl -o ..\third_party\idasdk84.zip https://files.quarkslab.com/45f34231-eac9-4b8a-9b39-52237b83515a/idasdk84.zip
-curl -o ..\third_party\idasdk90sp1.zip https://files.quarkslab.com/4b1d22ac-209e-46f8-bed1-df32a7d772d1/idasdk90sp1.zip
+curl -L -o ..\third_party\idasdk74.zip http://files.quarkslab.com/22262c8a-f73c-4571-b7cb-7d2b0abdf8df/idasdk74.zip
+curl -L -o ..\third_party\idasdk77.zip https://files.quarkslab.com/c6116218-52da-4c7d-b7ad-07a19fd6b075/idasdk77.zip
+curl -L -o ..\third_party\idasdk80.zip https://files.quarkslab.com/b91adc05-abdc-44ef-a1e2-6a3ccccc4c24/idasdk80.zip
+curl -L -o ..\third_party\idasdk81.zip https://files.quarkslab.com/cf1c6814-6304-466b-afa7-e02dab13c456/idasdk81.zip
+curl -L -o ..\third_party\idasdk82.zip https://files.quarkslab.com/96048f23-abb7-4f13-aeda-38ac156cea4e/idasdk82.zip
+curl -L -o ..\third_party\idasdk83.zip https://files.quarkslab.com/c2c793ae-7a42-4c91-aee7-725547547d3e/idasdk83.zip
+curl -L -o ..\third_party\idasdk84.zip https://files.quarkslab.com/45f34231-eac9-4b8a-9b39-52237b83515a/idasdk84.zip
+curl -L -o ..\third_party\idasdk90sp1.zip https://files.quarkslab.com/4b1d22ac-209e-46f8-bed1-df32a7d772d1/idasdk90sp1.zip
 
 echo Download complete.
