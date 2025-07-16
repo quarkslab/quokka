067cac74f81a1711b1ce8b74b9f49d501aa5721e
Riccardo Mori <rmori@quarkslab.com>
2025-07-16 16:45:48 +0200

    Add download links for ida sdk 8.5

--- scripts/fetch_sdk.sh
+++ scripts/fetch_sdk.sh
@@ -8,6 +8,7 @@ wget -O ../third_party/idasdk81.zip https://files.quarkslab.com/cf1c6814-6304-46
 wget -O ../third_party/idasdk82.zip https://files.quarkslab.com/96048f23-abb7-4f13-aeda-38ac156cea4e/idasdk82.zip
 wget -O ../third_party/idasdk83.zip https://files.quarkslab.com/c2c793ae-7a42-4c91-aee7-725547547d3e/idasdk83.zip
 wget -O ../third_party/idasdk84.zip https://files.quarkslab.com/45f34231-eac9-4b8a-9b39-52237b83515a/idasdk84.zip
+wget -O ../third_party/idasdk85.zip https://files.quarkslab.com/736e0826-a312-4627-9e6d-d9c747f1d324/idasdk85.zip
 wget -O ../third_party/idasdk90.zip https://files.quarkslab.com/2fe96720-93f5-46f2-a476-69a0304df901/idasdk90.zip
 wget -O ../third_party/idasdk90sp1.zip https://files.quarkslab.com/4b1d22ac-209e-46f8-bed1-df32a7d772d1/idasdk90sp1.zip
 wget -O ../third_party/idasdk91.zip https://files.quarkslab.com/f773930b-3c20-4974-992b-472b88223cd9/idasdk91.zip
